e3bf71d718bbd62c8b8e63175037e4772b6aa0dc
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-09 19:31:42 +0200

    add WebGPU initialization and canvas configuration

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -5,3 +5,24 @@ if (!navigator.gpu) {
 } else {
   console.log("WebGPU je podržan!");
 }
+async function initWebGPU() {
+  // 1. Dobijanje adaptera (GPU)
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) return;
+
+  // 2. Dobijanje uređaja
+  const device = await adapter.requestDevice();
+
+  // 3. Konfiguracija Canvas-a
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  const context = canvas.getContext("webgpu")!;
+
+  context.configure({
+    device,
+    format: navigator.gpu.getPreferredCanvasFormat(),
+  });
+
+  console.log("WebGPU je spreman!", device);
+}
+
+initWebGPU();
