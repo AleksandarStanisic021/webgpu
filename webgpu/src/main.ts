f1d1b95b862dafaf4f1b6f36da6f4e0444d0e720
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-11 20:53:07 +0200

    fixes

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -1,24 +1,39 @@
 import "./style.css";
 
-async function initWebGPU() {
-  // 1. Dobijanje adaptera (GPU)
-  const adapter = await navigator.gpu.requestAdapter();
-  if (!adapter) return;
+async function init() {
+  if (!navigator.gpu) {
+    throw new Error("WebGPU is not supported in this browser.");
+  }
+  const adapter = await navigator.gpu.requestAdapter({});
 
-  // 2. Dobijanje uređaja
+  if (!adapter) {
+    throw new Error("Failed to get GPU adapter.");
+  }
+
   const device = await adapter.requestDevice();
 
-  // 3. Konfiguracija Canvas-a
   const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error("Canvas element not found.");
+  }
 
-  const context = canvas.getContext("webgpu")!;
+  const context = canvas.getContext("webgpu") as GPUCanvasContext;
+  if (!context) {
+    throw new Error("Failed to get WebGPU context.");
+  }
 
+  const format = navigator.gpu.getPreferredCanvasFormat();
   context.configure({
     device,
-    format: navigator.gpu.getPreferredCanvasFormat(),
+    format,
   });
 
-  console.log("WebGPU je spreman!", device);
+  return { device, context, format };
 }
 
-await initWebGPU();
+async function run() {
+  const { device, context, format } = await init();
+  console.log("i do run");
+}
+
+run();
