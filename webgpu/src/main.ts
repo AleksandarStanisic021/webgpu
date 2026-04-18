5e37e7f9ab1c6436da5a29ab216de22e580a636a
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-18 15:48:08 +0200

    ading simple shader

--- webgpu/src/main.ts
+++ webgpu/src/main.ts
@@ -35,5 +35,61 @@ async function init() {
 async function run() {
   const { device, context, format, adapter } = await init();
   console.log("I do run...", device);
+  console.log("Adapter:", adapter);
+  console.log("Context:", context);
+  console.log("Format:", format);
+  console.log("Device:", device);
+  let pipeline = await device.createRenderPipelineAsync({
+    layout: "auto",
+    vertex: {
+      module: device.createShaderModule({
+        code: `
+        @vertex 
+        fn main(@builtin(vertex_index) vertexIndex : u32) -> @builtin(position) vec4<f32> {
+          var pos = array<vec2<f32>, 3>(
+            vec2<f32>(0.0, 0.5),
+            vec2<f32>(-0.5, -0.5),
+            vec2<f32>(0.5, -0.5)
+          );
+          return vec4<f32>(pos[vertexIndex], 0.0, 1.0);
+        }
+      `,
+      }),
+      entryPoint: "main",
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: `
+        @fragment
+        fn main() -> @location(0) vec4<f32> {
+          return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+        }
+      `,
+      }),
+      entryPoint: "main",
+      targets: [{ format }],
+    },
+    primitive: {
+      topology: "triangle-list",
+    },
+  });
+
+  const commandEncoder = device.createCommandEncoder();
+  const textureView = context.getCurrentTexture().createView();
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  };
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+  passEncoder.setPipeline(pipeline);
+  passEncoder.draw(3, 1, 0, 0);
+  passEncoder.end();
+  device.queue.submit([commandEncoder.finish()]);
 }
 run().catch((err) => console.error(err));
